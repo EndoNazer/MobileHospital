fbd90d4843d4fd61eb6e442f64b06e68ceae4255
Daniil <rodindaniil@gmail.com>
2019-11-19 12:29:05 +0400

    Initial Commit

---
+++ MobileHospital/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileHospital
+//
+//  Created by Даниил on 19.11.2019.
+//  Copyright © 2019 Даниил. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
